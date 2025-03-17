7cd9e8028fd19428b1fb95cf9bcf7f2eb3db30a9
Tako <46840416+takosenpai2687@users.noreply.github.com>
2025-03-17 15:15:01 +0800

    feat: test failures

--- tests/bstack_local_test.js
+++ tests/bstack_local_test.js
@@ -1,7 +1,19 @@
-const { expect, test } = require('@playwright/test');
-const sleep = require('util').promisify(setTimeout)
-test('BStack local is working', async ({ page }) => {
-  await page.goto('http://bs-local.com:45454/');
-  const title = await page.title();
-  expect(title).toContain('BrowserStack Local');
+const { expect, test } = require("@playwright/test");
+const sleep = require("util").promisify(setTimeout);
+test("BStack local is working", async ({ page }) => {
+    await page.goto("http://bs-local.com:45454/");
+    const title = await page.title();
+    expect(title).toContain("BrowserStack Local");
+});
+
+test("Deliberately fail to trigger CI failure strategy", async () => {
+    await page.goto("http://bs-local.com:45454/");
+    const title = await page.title();
+    expect(title).toContain("FFFFFFFF");
+});
+
+test("Deliberately throw to trigger CI failure strategy", async () => {
+    await page.goto("http://bs-local.com:45454/");
+    const title = await page.title();
+    throw new Error("Got a runtime error");
 });

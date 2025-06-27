3474a56dab9d6f1237a82fa4f32616c99f360cd1
Roberto Segura - phproberto <roberto@phproberto.com>
2025-06-27 13:45:39 +0200

    [imp] clean tests

--- tests/bstack_test_add_to_cart.js
+++ tests/bstack_test_add_to_cart.js
@@ -12,7 +12,7 @@ test('BStackDemo test add to cart', async ({ page }) => {
 
   // get name of item in cart
   const productInCart = await page.textContent('#__next > div > div > div.float-cart.float-cart--open > div.float-cart__content > div.float-cart__shelf-container > div > div.shelf-item__details > p.title');
-  
+
   // check if product in cart is same as one added
   expect(productInCart).toEqual(productToAdd);
 });
